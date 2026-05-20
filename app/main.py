b2276b2c4e9f3a7ca5e68670b668622a649c9f16
phash-babs <fasogbonbabatunde88@gmail.com>
2026-05-20 18:34:33 +0100

    temporary cors fix

--- app/main.py
+++ app/main.py
@@ -27,10 +27,7 @@ app= FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://clinic-management-system-mzen.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
