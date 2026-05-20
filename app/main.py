cccaec67c102ace3ae364a0c03edca4b0a34075a
phash-babs <fasogbonbabatunde88@gmail.com>
2026-05-20 18:40:43 +0100

    secure production cors

--- app/main.py
+++ app/main.py
@@ -27,7 +27,10 @@ app= FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://clinic-management-system-8aev.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
